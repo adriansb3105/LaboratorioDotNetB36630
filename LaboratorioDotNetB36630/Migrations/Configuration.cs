870b63b440de744fa888c1f7e4bd689ba159f7b2
Adrian Serrano <adriansb3105@gmail.com>
2018-07-02 23:54:44 -0600

    Tabs to see ordered data is added

--- LaboratorioDotNetB36630/Migrations/Configuration.cs
+++ LaboratorioDotNetB36630/Migrations/Configuration.cs
@@ -21,11 +21,11 @@ namespace LaboratorioDotNetB36630.Migrations
             //  to avoid creating duplicate seed data.
 
             /*Vehiculo vehiculo = new Vehiculo();
-            vehiculo.NumPlaca = "12345";
-            vehiculo.Color = "Negro";
-            vehiculo.Marca = "Sedane";
-            vehiculo.Estilo = "Convertible";
-            vehiculo.Anio = new DateTime(2010, 01, 01);
+            vehiculo.NumPlaca = "29920";
+            vehiculo.Color = "Rojo";
+            vehiculo.Marca = "Toyota";
+            vehiculo.Estilo = "Land Rover";
+            vehiculo.Anio = new DateTime(1987, 01, 01);
             vehiculo.Potencia = 500;
             vehiculo.Cilindraje = 1500;
             vehiculo.Capacidad = 4;
@@ -35,11 +35,11 @@ namespace LaboratorioDotNetB36630.Migrations
             vehiculo.Observaciones = "Vehiculo en buenas condiciones";
 
             Vehiculo vehiculo2 = new Vehiculo();
-            vehiculo2.NumPlaca = "23476";
-            vehiculo2.Color = "Negro";
-            vehiculo2.Marca = "Susuky";
-            vehiculo2.Estilo = "4x4";
-            vehiculo2.Anio = new DateTime(2000, 01, 01);
+            vehiculo2.NumPlaca = "48648";
+            vehiculo2.Color = "Plateado";
+            vehiculo2.Marca = "Mitsubishi";
+            vehiculo2.Estilo = "Automovil";
+            vehiculo2.Anio = new DateTime(2005, 01, 01);
             vehiculo2.Potencia = 400;
             vehiculo2.Cilindraje = 2500;
             vehiculo2.Capacidad = 5;
@@ -48,8 +48,38 @@ namespace LaboratorioDotNetB36630.Migrations
             vehiculo2.NumMotor = 2134;
             vehiculo2.Observaciones = "Vehiculo en buenas condiciones";
 
+            Vehiculo vehiculo3 = new Vehiculo();
+            vehiculo3.NumPlaca = "75654";
+            vehiculo3.Color = "Rojo";
+            vehiculo3.Marca = "Toyota";
+            vehiculo3.Estilo = "Jeep";
+            vehiculo3.Anio = new DateTime(2005, 01, 01);
+            vehiculo3.Potencia = 400;
+            vehiculo3.Cilindraje = 2500;
+            vehiculo3.Capacidad = 5;
+            vehiculo3.Peso = 1320;
+            vehiculo3.NumChasis = 2349;
+            vehiculo3.NumMotor = 2134;
+            vehiculo3.Observaciones = "Vehiculo en buenas condiciones";
+
+            Vehiculo vehiculo4 = new Vehiculo();
+            vehiculo4.NumPlaca = "9574734";
+            vehiculo4.Color = "Amarillo";
+            vehiculo4.Marca = "Nisaan";
+            vehiculo4.Estilo = "Centra";
+            vehiculo4.Anio = new DateTime(2005, 01, 01);
+            vehiculo4.Potencia = 400;
+            vehiculo4.Cilindraje = 2500;
+            vehiculo4.Capacidad = 5;
+            vehiculo4.Peso = 1320;
+            vehiculo4.NumChasis = 2349;
+            vehiculo4.NumMotor = 2134;
+            vehiculo4.Observaciones = "Vehiculo en buenas condiciones";
+
             context.Vehiculos.AddOrUpdate(x => x.NumPlaca, vehiculo);
-            context.Vehiculos.AddOrUpdate(x => x.NumPlaca, vehiculo2);*/
+            context.Vehiculos.AddOrUpdate(x => x.NumPlaca, vehiculo2);
+            context.Vehiculos.AddOrUpdate(x => x.NumPlaca, vehiculo3);
+            context.Vehiculos.AddOrUpdate(x => x.NumPlaca, vehiculo4);*/
         }
     }
 }
